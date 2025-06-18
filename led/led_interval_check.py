8bfb87050c3cd30190f8eba6e41b8a005bcfd7b8
EmuraFuzuki <emura.fuzuki.q5@dc.tohoku.ac.jp>
2025-06-18 14:42:58 +0900

    LEDクラスのtoggle_ledメソッドにinterval引数を追加し、点滅間隔を柔軟に設定できるように修正

--- led/led_interval_check.py
+++ led/led_interval_check.py
@@ -7,14 +7,14 @@ class LED:
         self.device = gpiozero.DigitalOutputDevice(pin)
         self.interval = interval
 
-    def toggle_led(self, time_length=10):
+    def toggle_led(self, time_length=10, interval=1.0):
         """LEDを指定した間隔で点滅させる関数"""
         end_time = time.time() + time_length
         while time.time() < end_time:
             self.device.on()
-            time.sleep(self.interval)
+            time.sleep(interval)
             self.device.off()
-            time.sleep(self.interval)
+            time.sleep(interval)
 
     def show_interval(self):
         """LEDの点滅間隔を表示する関数"""
@@ -24,8 +24,8 @@ class LED:
 def main():
     led = LED(23)
     for i in range(5):
-        time_length = 1 / (i + 1)  # 点滅時間を10秒に設定
-        led.toggle_led(time_length=time_length)
+        interval = 1 / (i + 1)  # 点滅時間を10秒に設定
+        led.toggle_led(interval=interval)
         led.show_interval()
 
 
