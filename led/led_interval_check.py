d92ed2b7f129ebe01a76c58fb79d7be520edf9d6
EmuraFuzuki <emura.fuzuki.q5@dc.tohoku.ac.jp>
2025-06-18 14:40:34 +0900

    LEDクラスを追加し、点滅機能をオブジェクト指向にリファクタリング

--- led/led_interval_check.py
+++ led/led_interval_check.py
@@ -5,18 +5,31 @@ LED_PIN = 23
 led = gpiozero.DigitalOutputDevice(LED_PIN)
 
 
-def toggle_led(time_length=10, interval=1):
-    """LEDを指定した間隔で点滅させる関数"""
-    end_time = time.time() + time_length
-    while time.time() < end_time:
-        led.on()
-        time.sleep(interval)
-        led.off()
-        time.sleep(interval)
+class LED:
+    def __init__(self, pin, interval=1.0):
+        self.device = gpiozero.DigitalOutputDevice(pin)
+        self.interval = interval
+
+    def toggle_led(self, time_length=10):
+        """LEDを指定した間隔で点滅させる関数"""
+        end_time = time.time() + time_length
+        while time.time() < end_time:
+            self.device.on()
+            time.sleep(self.interval)
+            self.device.off()
+            time.sleep(self.interval)
+
+    def show_interval(self):
+        """LEDの点滅間隔を表示する関数"""
+        print(f"LED is blinking every {self.interval:.2f} seconds.")
 
 
 def main():
-    toggle_led(time_length=10, interval=1)
+    led = LED(pin=LED_PIN)
+    for i in range(5):
+        time_length = 1 / (i + 1)  # 点滅時間を10秒に設定
+        led.toggle_led(time_length=time_length)
+        led.show_interval()
 
 
 if __name__ == "__main__":
