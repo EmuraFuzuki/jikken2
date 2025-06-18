718c870671d18f705614c2ea05bad22682c83762
EmuraFuzuki <emura.fuzuki.q5@dc.tohoku.ac.jp>
2025-06-18 14:45:28 +0900

    LEDクラスのインスタンス生成時にinterval引数を追加し、初期点滅間隔を設定できるように修正

--- led/led_interval_check.py
+++ led/led_interval_check.py
@@ -7,14 +7,18 @@ class LED:
         self.device = gpiozero.DigitalOutputDevice(pin)
         self.interval = interval
 
-    def toggle_led(self, time_length=10, interval=1.0):
+    def toggle_led(self, time_length=None, interval=None):
         """LEDを指定した間隔で点滅させる関数"""
-        end_time = time.time() + time_length
+        if time_length is not None:
+            self.time_length = time_length
+        if interval is not None:
+            self.interval = interval
+        end_time = time.time() + self.time_length
         while time.time() < end_time:
             self.device.on()
-            time.sleep(interval)
+            time.sleep(interval if interval is not None else self.interval)
             self.device.off()
-            time.sleep(interval)
+            time.sleep(interval if interval is not None else self.interval)
 
     def show_interval(self):
         """LEDの点滅間隔を表示する関数"""
@@ -22,7 +26,7 @@ class LED:
 
 
 def main():
-    led = LED(23)
+    led = LED(23, interval=1.0, time_length=3.0)  # LEDのピン番号と初期点滅間隔を設定
     for i in range(5):
         interval = 1 / (i + 1)  # 点滅時間を10秒に設定
         led.toggle_led(interval=interval)
