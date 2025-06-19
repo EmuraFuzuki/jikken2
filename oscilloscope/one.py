de2765930fb07c28690cecc83aeedbf1e70683dd
EmuraFuzuki <emura.fuzuki.q5@dc.tohoku.ac.jp>
2025-06-19 15:44:46 +0900

    main1関数を更新し、接続可能な機器のリストを表示する機能を追加

--- oscilloscope/one.py
+++ oscilloscope/one.py
@@ -1,3 +1,8 @@
 def main1():
-    while True:
-        print("continue")
+    import pyvisa
+
+    rm = pyvisa.ResourceManager()
+    vis_list = rm.list_resources()
+    print("接続可能な機器:")
+    for vis in vis_list:
+        print(vis)
