147727f7ef3020558e15c62a2c850f40241b6c18
EmuraFuzuki <emura.fuzuki.q5@dc.tohoku.ac.jp>
2025-06-25 13:20:32 +0900

    main1関数を追加し、接続可能な機器のリストを表示し、機器のIDを取得する機能を実装

---
+++ oscilloscope/two.py
@@ -0,0 +1,14 @@
+# 課題26
+def main1():
+    import pyvisa
+
+    rm = pyvisa.ResourceManager()
+    vis_list = rm.list_resources()
+    print("接続可能な機器:")
+    for vis in vis_list:
+        print(vis)
+
+    inst = rm.open_resource("USB0::6833::1100::DHO9A270600106::0::INSTR")
+
+    print(inst.query("*IDN?"))  # 機器のIDを取得
+    inst.write(":SINGle")
